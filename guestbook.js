b434b5abfb9cfb754b9327d6f6ff76c76cef5af0
Dominic Strasser <Dominic.Strasser@almtal.net>
2015-10-21 18:45:50 +0200

    hfghfghfgh

--- guestbook.js
+++ guestbook.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+var pg = require('pg');
+var conString = "postgres://xxedceoimtlzhm:fxEHniw4k0jdmAt_QtTjDW6eOE@ec2-54-204-15-48.compute-1.amazonaws.com:5432/d7941jfn7j2bm8";
 var app = express();
 var params;
-
-var pool  = mysql.createPool({
-    connectionLimit : 10,
-    host : 'ec2-54-204-15-48.compute-1.amazonaws.com',
-    port : 5432,
-    user : 'xxedceoimtlzhm',
-    password : 'fxEHniw4k0jdmAt_QtTjDW6eOE',
-    database : 'd7941jfn7j2bm8'
-});
 
 // Add headers
 app.use(function (req, res, next) {
@@ -41,28 +33,45 @@ app.post('/', function(req, res) {
     console.log('POST');
     
     if (params['mod'] == "set") {
-        pool.query("insert into gb_entries (name, ort, eintrag) values ( '"+params['name']+"', '"+params['ort']+"', '"+params['eintrag']+"' )", function(err, result) {
-            if (!err)
-                console.log(params);
-            else
-                console.log(err);
+        pg.connect(conString, function(err, client, done) {
+          if(err) {
+            return console.error('error fetching client from pool', err);
+          }
+          client.query("insert into gb_entries (name, ort, eintrag) values ( '"+params['name']+"', '"+params['ort']+"', '"+params['eintrag']+"' )", function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+              return console.error('error running query', err);
+            }
+          });
         });
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('done');
     } else if (params['mod'] == "get") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         console.log("Geladene Gaestebucheintraege:");
-        pool.query("select * from gb_entries order by datum desc", function(err, rows, fields) {
-            if (err) throw err;
-
-            for (var i in rows) {
-                console.log(rows[i]);
+        pg.connect(conString, function(err, client, done) {
+          if(err) {
+            return console.error('error fetching client from pool', err);
+          }
+          client.query("select * from gb_entries order by datum desc", function(err, result) {
+            if(err) {
+              return console.error('error running query', err);
+            }
+            for (var i in result.rows) {
+                console.log(result.rows[i]);
                 
-                var date = new Date(rows[i]['datum']);
-                res.write("<div class='panel panel-primary'><div class='panel-heading '>"+rows[i]['name']+" aus "+rows[i]['ort']+" am "+date.getDate()+"."+((date.getMonth()+1) < 10 ? ("0"+(date.getMonth()+1)) : (date.getMonth()+1))+"."+date.getFullYear()+" "+(date.getHours() < 10 ? ("0"+date.getHours()) : date.getHours())+":"+(date.getMinutes() < 10 ? ("0"+date.getMinutes()) : date.getMinutes()) +"</div><div class='panel-body'>"+rows[i]['eintrag']+"</div></div>");
+                var date = new Date(result.rows[i]['datum']);
+                res.write("<div class='panel panel-primary'><div class='panel-heading '>"+result.rows[i]['name']+" aus "+result.rows[i]['ort']+" am "+date.getDate()+"."+((date.getMonth()+1) < 10 ? ("0"+(date.getMonth()+1)) : (date.getMonth()+1))+"."+date.getFullYear()+" "+(date.getHours() < 10 ? ("0"+date.getHours()) : date.getHours())+":"+(date.getMinutes() < 10 ? ("0"+date.getMinutes()) : date.getMinutes()) +"</div><div class='panel-body'>"+result.rows[i]['eintrag']+"</div></div>");
             }
             res.end();
-        });
+              
+                
+            //call `done()` to release the client back to the pool
+            done();
+          });
+      });
     }
 });
 
