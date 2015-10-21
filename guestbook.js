bccaabe94d9e263c36420db51519edb8dfb5c6f1
Dominic Strasser <Dominic.Strasser@almtal.net>
2015-10-21 20:03:24 +0200

    dsdcsdc

--- guestbook.js
+++ guestbook.js
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, html');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
